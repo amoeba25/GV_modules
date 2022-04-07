376bf39a36eaf3b62399d128cf199ef3846d08d6
amoeba25 <shah.soham63@nmims.edu.in>
2022-04-07 12:08:12 +0530

    added postgres db

--- core/core/settings.py
+++ core/core/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'gv_modules',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
