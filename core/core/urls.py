af9e5c6c6a62eca243494005b291072364ce4ea2
amoeba25 <shah.soham63@nmims.edu.in>
2022-05-26 11:20:27 +0530

    added files

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,8 +1,13 @@
+import imp
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onboarding/', include('cust_onboard.urls')),  # customer onboarding url 
-    
-]
+    path('leave/', include('leave_manage.urls'))
+    # path('detection/', include('video_detect.urls')), 
+    # path('production/', include('production.urls')), 
+] + static(settings.MEDIA_URL, document_root  = settings.MEDIA_ROOT)
